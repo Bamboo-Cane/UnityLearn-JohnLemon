5c040fb113de286a6d69ffc732e99eacd3f1dd71
Bamboo-Cane <kangzheng1999@hotmail.com>
2021-06-18 21:16:59 +0800

    Add project

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float fadeDuration = 2f;
+    public float displayImageDuration = 2f;
+    public GameObject player;
+    public CanvasGroup exitBackgoundImageCanvasGroup;
+    public AudioSource exitAudio;
+    public CanvasGroup caughtBackGroundImageCanvasGroup;
+    public AudioSource caughtAudio;
+
+    bool m_IsPlayerExit;
+    bool m_IsPlayerCaught;
+    float m_Timer;
+    bool m_hasAudioPlayed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            m_IsPlayerExit = true;
+        }
+    }
+
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+    private void Update()
+    {
+        if (m_IsPlayerExit)
+        {
+            EndLevel(exitBackgoundImageCanvasGroup, false, exitAudio);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackGroundImageCanvasGroup, true, caughtAudio);
+        }
+    }
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doStart, AudioSource audioSource)
+    {
+        if (!m_hasAudioPlayed)
+        {
+            audioSource.Play();
+            m_hasAudioPlayed = true;
+        }
+
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if(m_Timer > fadeDuration + displayImageDuration)
+        {
+            if (doStart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Joystick joystick;
+    public float turnSpeed = 20f;
+
+    public float leftOrRight = 0f;
+    public float upOrDown = 0f;
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        leftOrRight = joystick.Horizontal;
+        upOrDown = joystick.Vertical;
+
+        m_Movement.Set(leftOrRight, 0f, upOrDown);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(leftOrRight, 0f);
+        bool hasVertical = !Mathf.Approximately(upOrDown, 0f);
+        bool isWalking = hasHorizontalInput || hasVertical;
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    private void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+}
